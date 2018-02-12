960b04552af6ba496989102151e3ed33364427f3
soushians noorghorbani <snoorghorbani@gmail.com>
2018-02-13 01:08:10 +0330

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+﻿export * from "./lib/utility.service";
+export * from "./lib/helper";
+export * from "./lib/validators";
+export * from "./lib/socket";
+
+export * from "./lib/infra.module";
---
+++ index.js
@@ -0,0 +1,5 @@
+﻿exports.InfraModule = require("./lib/infra.module");
+exports.InfraModule = require("./lib/utility.service");
+exports.InfraModule = require("./lib/helper");
+exports.InfraModule = require("./lib/validators");
+exports.InfraModule = require("./lib/socket");
