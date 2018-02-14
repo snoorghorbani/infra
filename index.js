5ab0b88a5d8029673084521b25532813b4c6b6e0
soushians noorghorbani <snoorghorbani@gmail.com>
2018-02-14 06:55:20 +0330

    1.0.39

--- index.js
+++
@@ -1,5 +0,0 @@
-﻿exports.InfraModule = require("./lib/infra.module").InfraModule;
-exports.UtilityService = require("./lib/utility.service").UtilityService;
-exports.helper = require("./lib/helper");
-exports.validators = require("./lib/validators");
-exports.socket = require("./lib/socket");
---
+++ lib/dist/infra.module.ngfactory.d.ts
@@ -0,0 +1,3 @@
+import * as i0 from '@angular/core';
+import * as i1 from './infra.module';
+export declare const InfraModuleNgFactory: i0.NgModuleFactory<i1.InfraModule>;
